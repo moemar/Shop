b73ef6e4491489a9018b26c952cbf2e3f52d264a
Martin Moen <magmo@eg.no>
2019-07-29 01:34:48 +0200

    Added Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,35 @@
-// _document is only rendered on the server side and not on the client side
-// Event handlers like onClick can't be added to this file
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
 
-// ./pages/_document.js
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
+export default class extends Document {
   render() {
     return (
-      <Html lang="no">
-        <Head />
+      <html lang="no">
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}');
+          `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </html>
     );
   }
 }
-
-export default MyDocument;
