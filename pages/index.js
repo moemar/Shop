2d631bcb2f9f42d1ba11b211762ce37a48e3cb22
Martin Moen <magmo@eg.no>
2019-07-29 01:18:07 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <meta name="description" content="Meta description" />
+      <title>Home</title>
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href="/static/favicon.ico"
+      />
+    </Head>
+  </div>
+);
+
+export default Home
